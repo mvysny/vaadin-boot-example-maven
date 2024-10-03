9c7f00c16c7254e123be543bd2661bf4599979a6
Martin Vysny <mavi@vaadin.com>
2024-10-03 11:54:32 +0300

    Implement background jobs

--- src/main/java/com/vaadin/starter/skeleton/Bootstrap.java
+++ src/main/java/com/vaadin/starter/skeleton/Bootstrap.java
@@ -1,8 +1,16 @@
 package com.vaadin.starter.skeleton;
 
+import com.vaadin.flow.component.UI;
 import jakarta.servlet.ServletContextEvent;
 import jakarta.servlet.ServletContextListener;
 import jakarta.servlet.annotation.WebListener;
+import org.jetbrains.annotations.NotNull;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+import java.util.Objects;
+import java.util.concurrent.*;
+import java.util.function.Consumer;
 
 /**
  * The {@link #contextInitialized(ServletContextEvent)} is called once,
@@ -11,15 +19,60 @@ import jakarta.servlet.annotation.WebListener;
  */
 @WebListener
 public class Bootstrap implements ServletContextListener {
+    private static final Logger log = LoggerFactory.getLogger(Bootstrap.class);
+
+    private static volatile ExecutorService executorService;
+
     @Override
     public void contextInitialized(ServletContextEvent sce) {
         // to be implemented by actual apps.
         // possibly setup database connections, or initialize background task executor
+        executorService = Executors.newSingleThreadExecutor();
     }
 
     @Override
     public void contextDestroyed(ServletContextEvent sce) {
-        // to be implemented by actual apps.
-        // possibly stop & kill background task executor, or cleanup temp files, or such.
+        try {
+            executorService.shutdown();
+            executorService.awaitTermination(10L, TimeUnit.SECONDS);
+        } catch (InterruptedException e) {
+            log.error("Error while shutting down background jobs", e);
+        }
+    }
+
+    private static final class UIExecutor implements Executor {
+        @NotNull
+        private final UI ui;
+
+        public UIExecutor() {
+            this.ui = Objects.requireNonNull(UI.getCurrent(), "must be called from Vaadin ui thread");
+        }
+
+        @Override
+        public void execute(@NotNull Runnable command) {
+            Objects.requireNonNull(command);
+            ui.access(command::run);
+        }
+    }
+
+    public interface Job {
+        /**
+         * Runs the job synchronously.
+         * @param uiExecutor used to update Vaadin UI.
+         * @throws Exception if the job fails.
+         */
+        void run(@NotNull Executor uiExecutor) throws Exception;
+    }
+
+    @NotNull
+    public static Future<?> submitBackgroundJob(@NotNull Job job) {
+        final Executor uiExecutor = new UIExecutor();
+        return executorService.submit(() -> {
+            try {
+                job.run(uiExecutor);
+            } catch (Throwable t) {
+                log.error("Error while executing background job", t);
+            }
+        });
     }
 }
--- src/main/java/com/vaadin/starter/skeleton/MainView.java
+++ src/main/java/com/vaadin/starter/skeleton/MainView.java
@@ -1,12 +1,18 @@
 package com.vaadin.starter.skeleton;
 
+import com.vaadin.flow.component.DetachEvent;
 import com.vaadin.flow.component.Key;
 import com.vaadin.flow.component.button.Button;
 import com.vaadin.flow.component.button.ButtonVariant;
-import com.vaadin.flow.component.notification.Notification;
+import com.vaadin.flow.component.html.Span;
 import com.vaadin.flow.component.orderedlayout.VerticalLayout;
 import com.vaadin.flow.component.textfield.TextField;
 import com.vaadin.flow.router.Route;
+import org.jetbrains.annotations.NotNull;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.concurrent.Future;
 
 /**
  * The main view contains a button and a click listener.
@@ -14,14 +20,17 @@ import com.vaadin.flow.router.Route;
 @Route("")
 public class MainView extends VerticalLayout {
 
+    private final Span statusSpan = new Span();
+    private final List<Future<?>> backgroundJobs = new ArrayList<>();
+
     public MainView() {
         // Use TextField for standard text input
         TextField textField = new TextField("Your name");
         textField.addClassName("bordered");
 
         // Button click listeners can be defined as lambda expressions
-        Button button = new Button("Say hello",
-                e -> Notification.show("Hello " + textField.getValue()));
+        Button button = new Button("Run background thread",
+                e -> startBackgroundJob(textField.getValue()));
 
         // Theme variants give you predefined extra styles for components.
         // Example: Primary button is more prominent look.
@@ -34,6 +43,25 @@ public class MainView extends VerticalLayout {
         // Use custom CSS classes to apply styling. This is defined in shared-styles.css.
         addClassName("centered-content");
 
-        add(textField, button);
+        add(textField, button, statusSpan);
+    }
+
+    @Override
+    protected void onDetach(DetachEvent detachEvent) {
+        backgroundJobs.forEach(it -> it.cancel(true));
+        backgroundJobs.clear();
+        super.onDetach(detachEvent);
+    }
+
+    private void startBackgroundJob(@NotNull final String name) {
+        statusSpan.setText("");
+        final Future<?> jobFuture = Bootstrap.submitBackgroundJob(uiExecutor -> {
+            uiExecutor.execute(() -> statusSpan.setText("Started job " + name + "! Thinking"));
+            Thread.sleep(1000L);
+            uiExecutor.execute(() -> statusSpan.setText("Thinking some more..."));
+            Thread.sleep(1000L);
+            uiExecutor.execute(() -> statusSpan.setText("Done!"));
+        });
+        backgroundJobs.add(jobFuture);
     }
 }
