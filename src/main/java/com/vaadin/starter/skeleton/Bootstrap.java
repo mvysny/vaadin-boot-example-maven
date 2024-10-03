fb55523aa38956eb982c528d72b4ef1f800928a5
Martin Vysny <mavi@vaadin.com>
2024-10-03 11:34:40 +0300

    javadoc

--- src/main/java/com/vaadin/starter/skeleton/Bootstrap.java
+++ src/main/java/com/vaadin/starter/skeleton/Bootstrap.java
@@ -4,6 +4,11 @@ import jakarta.servlet.ServletContextEvent;
 import jakarta.servlet.ServletContextListener;
 import jakarta.servlet.annotation.WebListener;
 
+/**
+ * The {@link #contextInitialized(ServletContextEvent)} is called once,
+ * before the Vaadin app receives any requests. The {@link #contextDestroyed(ServletContextEvent)}
+ * is called exactly once when the app is shutting down.
+ */
 @WebListener
 public class Bootstrap implements ServletContextListener {
     @Override
@@ -15,6 +20,6 @@ public class Bootstrap implements ServletContextListener {
     @Override
     public void contextDestroyed(ServletContextEvent sce) {
         // to be implemented by actual apps.
-        // possibly stop & kill background task executor
+        // possibly stop & kill background task executor, or cleanup temp files, or such.
     }
 }
